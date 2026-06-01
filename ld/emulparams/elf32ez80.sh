599710516e80cb75042bd2a72472068844bfffb8
beattidp <5289980+beattidp@users.noreply.github.com>
2026-05-31 23:51:18 -0600

    ld: Add eZ80 target configuration and ELF emulation parameters

--- ld/emulparams/elf32ez80.sh
+++ ld/emulparams/elf32ez80.sh
@@ -2,7 +2,7 @@
 #  * @file elf32ez80.sh
 #  * @brief Linker emulation parameters for the eZ80 ELF target.
 #  */
-TEMPLATE_NAME=elf32
+TEMPLATE_NAME=elf
 SCRIPT_NAME=elf
 OUTPUT_FORMAT="elf32-ez80"
 OUTPUT_ARCH="ez80"
